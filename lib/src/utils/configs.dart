3b30e6c67d9a79091a93bea23ac88898d7644e1c
MO-SALAH-Daia <mohammad.daia@gmail.com>
2021-03-29 14:26:47 +0200

    first commit

---
+++ lib/src/utils/configs.dart
@@ -0,0 +1,12 @@
+// const String APP_ID = "476";
+// const String AUTH_KEY = "PDZjPBzAO8WPfCp";
+// const String AUTH_SECRET = "6247kjxXCLRaua6";
+// const String ACCOUNT_ID = "TpuBZox_HPxofh7PVZdP";
+// const String DEFAULT_PASS = "xxasBUM3gQs36bhj";
+
+const String APP_ID = "4484";
+const String AUTH_KEY = "LWY8zyD4MReYve6";
+const String AUTH_SECRET = "XJ5ZEDDYwCMXeNs";
+const String ACCOUNT_ID = "S82q8Hp1ST67sk7WQCGg";
+// const String DEFAULT_PASS = "12345678";
+// const String DEFAULT_PASS = "xxasBUM3gQs36bhj";
